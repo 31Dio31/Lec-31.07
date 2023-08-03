875d8d2eb5a026bdefe476929f482405b3cb8444
Your Name <you@example.com>
2023-08-03 21:00:02 +0300

    1

---
+++ Практика1/Program.cs
@@ -0,0 +1,10 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+if (num1 * num1 ==num2)
+{
+    Console.WriteLine("Число 2 явялется квадратом числа 1");
+}
+else
+{
+    Console.WriteLine("Число 2 не явялется квадратом числа 1");
+}
