419ac2a84a9c9d5b383656109b409cc82f50b203
Your Name <you@example.com>
2023-08-04 20:08:10 +0300

    1

---
+++ Практика2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+int num1 = int.Parse (Console.ReadLine());
+if (num1 == 1)
+    Console.Writeline("Понедельник");
+if (num1 == 2)
+    Console.Writeline("Вторник");
+if (num1 == 3)
+    Console.Writeline("Среда");
+if (num1 == 4)
+    Console.Writeline("Четверг");
+if (num1 == 5)
+    Console.Writeline("Пятница");
+if (num1 == 6)
+    Console.Writeline("Суббота");
+if (num1 == 7)
+    Console.Writeline("Воскресенье");
